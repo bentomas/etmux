3d7494cdb7a5145c8f5661e68d01888fb4b478d8
Benjamin Thomas <ben@bentomas.com>
2016-10-16 22:08:11 -0600

    update etmux-session-finder script to show etmux sessions

--- etmux-session-finder.sh
+++ etmux-session-finder.sh
@@ -1,36 +1,63 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # from https://github.com/siadat/session-finder
 
 set -e
-prompt="find/create session> "
-tmux='/usr/bin/env tmux'
 
-fzf_out=$($tmux ls -F '#{session_attached} #{?session_last_attached,,0}#{session_last_attached} #{session_name}' | grep -v '^1' | sort -r | perl -pe 's/^[0-9]+ [0-9]+//' | fzf --print-query --prompt="$prompt" || true)
+file_sessions() {
+  local socket
+  local sessions
+  local paths
+  local etmux_path
+  local files
+
+  if [ -z $EASY_TMUX_PATH ]; then
+    etmux_path=~/.etmux-sessions
+  else
+    etmux_path=$EASY_TMUX_PATH
+  fi
+
+  paths=(`echo $etmux_path | tr ":" "\n"`)
+  for x in $paths; do
+    if [ -d "$x" ]; then
+      cd $x
+      ls -1 $x
+    fi
+  done
+}
+
+active_sessions() {
+  tmux ls -F '#{session_attached} #{?session_last_attached,,0}#{session_last_attached} #{session_name}' | sort -r | perl -pe 's/^[0-9]+ [0-9]+ //'
+}
+
+sessions() {
+  sessions=("${(@f)$(active_sessions)}")
+  files=("${(@f)$(file_sessions)}")
+
+  for s in $sessions; do
+    files=("${(@)files:#$s}")
+  done
+
+  if [ "$TMUX" ]; then
+    current=$sessions[1]
+    sessions=("${(@)sessions:#$current}")
+  fi
+
+  for s in $sessions; do
+   echo "$s *"
+  done
+  for f in $files; do
+   echo $f
+  done
+}
+
+prompt="find/create session> "
+
+fzf_out=$(sessions | fzf --print-query --prompt="$prompt" || true)
 line_count=$(echo "$fzf_out" | wc -l)
 session_name=$(echo "$fzf_out" | tail -n1)
 command=$(echo "$session_name" | awk '{ print $1 }')
 
-if [ $line_count -eq 1 ]; then
-  unset TMUX
-  word_count=$(echo "$fzf_out" | wc -w)
-  if [ $word_count -eq 1 ]; then
-    $tmux new-session -d -s $session_name
-    $tmux switch-client -t $session_name
-  else
-    session_name=$(echo "$fzf_out" | tail -n1 | awk '{ print $2 }')
-    case "$command" in
-      ":new")
-        $tmux new-session -d -s $session_name
-        $tmux switch-client -t $session_name
-        ;;
-      ":rename")
-        $tmux rename-session $session_name
-        ;;
-    esac
-  fi
-else
-  $tmux switch-client -t $session_name
+if [ "$session_name" ]; then
+ etmux $session_name
 fi
-sleep 0.1
-$tmux refresh-client -S
